f2f00b1f41ceff66035c12f6cf7f7a8ffb92afd1
alvezandreluiz <alvezandreluiz@gmail.com>
2024-08-18 20:46:25 -0300

    Implementação da camada de dominio

---
+++ src/main/java/dio_rest_api_na_nuvem/domain/model/News.java
@@ -0,0 +1,8 @@
+package dio_rest_api_na_nuvem.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
---
+++ src/main/java/dio_rest_api_na_nuvem/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dio_rest_api_na_nuvem.domain.repository;
+
+import dio_rest_api_na_nuvem.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
