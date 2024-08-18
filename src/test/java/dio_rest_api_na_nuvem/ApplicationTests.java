00919eb3d8d8f76b5a3b8cc99a37f7f1f3221a49
alvezandreluiz <alvezandreluiz@gmail.com>
2024-08-18 16:36:01 -0300

    Initial commit

---
+++ src/test/java/dio_rest_api_na_nuvem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio_rest_api_na_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
